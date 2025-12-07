7aa1ecac4088f5e473379988bb37b065d54db29c
ava-garrett-usf <arg25@usf.edu>
2025-12-07 14:55:10 -0500

    Fixed Data import printed head to verify

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,4 +1,11 @@
 #Install required libraries (pandas)
 import pandas as pd
+
 #Import the raw data csv file
-raw_data = pd.read_csv('raw_data.csv')
+raw_data = pd.read_csv('data/raw/sales_data_raw.csv')
+
+#Show the first few rows of the raw data
+print("Raw Data Preview:")
+print(raw_data.head())
+
+
